366c0eda6590ece6c0986776d3d5e8272608d960
jaythree <giljr.2009@gmail.com>
2023-05-13 19:16:23 -0400

    until vid:  100. DRY - refactoring and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :set_article, only: [ :show, :edit, :update, :destroy ]
 
   # GET /articles or /articles.json
   def index
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
   # GET /articles/1 or /articles/1.json
   def show
     # byebug
-    article = Article.find(params[:id])
+    # article = Article.find(params[:id])
   end
 
   # GET /articles/new
@@ -19,13 +19,14 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
-    article = Article.find(params[:id])
+    # article = Article.find(params[:id])
   end
 
   # POST /articles or /articles.json
   # # render plain: params[:article].inspect -> not working?
-  def create       
-    @article = Article.new(params.require(:article).permit(:title, :description)) 
+  def create   
+    @article = Article.new(article_params)     
+    # @article = Article.new(params.require(:article).permit(:title, :description)) 
     if @article.save
       flash[:notice] = "Article was created sucessfully!"
     # GET "/articles/12"
@@ -39,24 +40,16 @@ class ArticlesController < ApplicationController
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description)) 
-      flash[:notice] = "Article was suucessfully updated!"
+    # @article = Article.find(params[:id])
+    if @article.update(article_params) 
+    # if @article.update(params.require(:article).permit(:title, :description)) 
+      flash[:notice] = "Article was sucessfully updated!"
       redirect_to @article  
     else
       render "edit" 
     end  
   end
 
-  # respond_to do |format|
-  #   if @article.update(article_params)
-  #     format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-  #     format.json { render :show, status: :ok, location: @article }
-  #   else
-  #     format.html { render :edit, status: :unprocessable_entity }
-  #     format.json { render json: @article.errors, status: :unprocessable_entity }
-  #   end
-  # end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
@@ -65,11 +58,6 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
-  # @article.destroy
-
-  # respond_to do |format|
-  #   format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-  #   format.json { head :no_content }
   
   private
     # Use callbacks to share common setup or constraints between actions.
